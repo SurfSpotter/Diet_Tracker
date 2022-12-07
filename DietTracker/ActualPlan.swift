b86f6db34a56a046703645eae66503f0c35d78cc
Aleksey Chigarskikh <SurfSpot>
2022-12-07 18:22:50 +0300

    main page view model added

---
+++ DietTracker/ActualPlan.swift
@@ -0,0 +1,12 @@
+//
+//  ActualPlan.swift
+//  DietTracker
+//
+//  Created by Алексей Чигарских on 07.12.2022.
+//
+
+import Foundation
+
+struct ActualPlan {
+    
+}
---
+++ DietTracker/MainPage/MainPageViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  MainPageViewModel.swift
+//  DietTracker
+//
+//  Created by Алексей Чигарских on 07.12.2022.
+//
+
+import Foundation
+
+struct MainPageViewModel {
+    var actualPlan: ActualPlan
+}
+
+
+
+
