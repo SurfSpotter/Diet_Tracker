a63421c0d83fd5d6d5db0369dcac5848e5927f70
Aleksey Chigarskikh <SurfSpot>
2022-12-07 17:48:47 +0300

    Initial Commit

---
+++ DietTracker/DietTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DietTrackerApp.swift
+//  DietTracker
+//
+//  Created by Алексей Чигарских on 07.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DietTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
